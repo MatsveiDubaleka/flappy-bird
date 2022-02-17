459516af84d889cd9bfb2192ae3450f8418194eb
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-02-17 16:07:07 +0300

    feat: add source to images

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ const bg = new Image();
 const fg = new Image();
 const pipeUp = new Image();
 const pipeBotoom = new Image();
+
+bird.src = "./assets/flappy_bird_bird.png";
+bg.src = "./assets/flappy_bird_bg.png";
+fg.src = "./assets/flappy_bird_fg.png";
+pipeUp.src = "./assets/flappy_bird_pipeUp.png";
+pipeBotoom.src = "./assets/flappy_bird_pipeBottom.png";
