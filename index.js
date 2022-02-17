0c474008be3230f9615772f223ddf9416ee96aee
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-02-17 16:10:47 +0300

    feat: add pipeUp and pipeDown

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ const bird = new Image();
 const bg = new Image();
 const fg = new Image();
 const pipeUp = new Image();
-const pipeBotoom = new Image();
+const pipeBottom = new Image();
 
 bird.src = "./assets/flappy_bird_bird.png";
 bg.src = "./assets/flappy_bird_bg.png";
 fg.src = "./assets/flappy_bird_fg.png";
 pipeUp.src = "./assets/flappy_bird_pipeUp.png";
-pipeBotoom.src = "./assets/flappy_bird_pipeBottom.png";
+pipeBottom.src = "./assets/flappy_bird_pipeBottom.png";
+
+let gap = 90;
 
 function draw() {
     ctx.drawImage(bg, 0, 0);
+
+    ctx.drawImage(pipeUp, 100, 0);
+    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap)
 }
 
 pipeBotoom.onload = draw;
