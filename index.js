a1e2c439d2977e772252d2ec59d114179c2d66da
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-02-17 16:03:04 +0300

    feat: add images to js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 let cvs = document.getElementById('canvas');
 let ctx = cvs.getContext('2d');
+
+const bird = new Image();
+const bg = new Image();
+const fg = new Image();
+const pipeUp = new Image();
+const pipeBotoom = new Image();
